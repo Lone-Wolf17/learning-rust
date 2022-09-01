948eb159453cbbf46506390fbf520b0c22256cfa
Lone Wolf <53178226+Lone-Wolf17@users.noreply.github.com>
2022-09-01 08:51:48 +0100

    ** convert server to a multi threaded server

--- rust_web_server/src/main.rs
+++ rust_web_server/src/main.rs
@@ -2,14 +2,22 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+
+use rust_web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("localhost:7878").unwrap();
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -26,9 +34,17 @@ fn handle_connection(mut stream: TcpStream) {
 
     let get = b"GET / HTTP/1.1\r\n";
 
+    // sleep is used to simulate a slow request
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK", "index.html")
-    } else {
+    }  else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")
+    }
+
+    else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
